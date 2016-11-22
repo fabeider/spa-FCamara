e009541282ef7e60a17f348ec3baaee14a1ace91
Fabio Beider <fabeider@gmail.com>
2016-11-22 15:17:41 -0200

    Add style to product list

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,25 +1,95 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `
-  <h1>{{title}}</h1>
-  <h2>{{product.name}} details!</h2>
-  <div><label>id: </label>{{product.id}}</div>
-  <div><label>name: </label>{{product.name}}</div>
-  `
+selector: 'my-app',
+template: `
+<h2>FCamara Products</h2>
+<ul class="products">
+  <li *ngFor="let product of products" (click)="onSelect(product)">
+    <span class="badge">{{product.id}}</span> {{product.name}}
+    <!-- each hero goes here -->
+  </li>
+</ul>  
+`, 
+styles: [`
+.selected {
+background-color: #CFD8DC !important;
+color: white;
+}
+.products {
+margin: 0 0 2em 0;
+list-style-type: none;
+padding: 0;
+width: 15em;
+}
+.products li {
+cursor: pointer;
+position: relative;
+left: 0;
+background-color: #EEE;
+margin: .5em;
+padding: .3em 0;
+height: 1.6em;
+border-radius: 4px;
+}
+.products li.selected:hover {
+background-color: #BBD8DC !important;
+color: white;
+}
+.products li:hover {
+color: #607D8B;
+background-color: #DDD;
+left: .1em;
+}
+.products .text {
+position: relative;
+top: -3px;
+}
+.products .badge {
+display: inline-block;
+font-size: small;
+color: white;
+padding: 0.8em 0.7em 0 0.7em;
+background-color: #607D8B;
+line-height: 1em;
+position: relative;
+left: -1px;
+top: -4px;
+height: 1.8em;
+margin-right: .8em;
+border-radius: 4px 0 0 4px;
+}
+`]
 })
 
 export class AppComponent {
-  title = 'FCamara products';
-  product: Product = {
-    id: 1,
-    name: 'Computer'
-    details: 'Computer to compute';
-  }
+title = 'FCamara products';
+products = PRODUCTS;
+}
 
 export class Product {
-  id: number;
-  name: string;
-  details: string;
-  }
+id: number;
+name: string;
+}
+
+const PRODUCTS: Product[] = [
+{ id: 11, name: 'Bola' },
+{ id: 12, name: 'Pasta de dente' },
+{ id: 13, name: 'Coca-cola' },
+{ id: 14, name: 'Café' },
+{ id: 15, name: 'Manteiga' },
+{ id: 16, name: 'Cerveja' },
+{ id: 17, name: 'Tomate' },
+{ id: 18, name: 'Celular' },
+{ id: 19, name: 'Arroz' },
+{ id: 20, name: 'Feijão' }
+];
+
+
+
+
+
+
+
+
+
