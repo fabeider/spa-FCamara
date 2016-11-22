68c2af866fa3b1b2402fe44e0a1dc55d1ded0ba4
Fabio Beider <fabeider@gmail.com>
2016-11-22 14:06:49 -0200

    Creste Product class

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>Hello Angular!</h1>'
+  template: `
+  <h1>{{title}}</h1>
+  <h2>{{product.name}} details!</h2>
+  <div><label>id: </label>{{product.id}}</div>
+  <div><label>name: </label>{{product.name}}</div>
+  `
 })
 
-export class AppComponent { }
+export class AppComponent {
+  title = 'FCamara products';
+  product: Product = {
+    id: 1,
+    name: 'Computer'
+    details: 'Computer to compute';
+  }
+
+export class Product {
+  id: number;
+  name: string;
+  details: string;
+  }
