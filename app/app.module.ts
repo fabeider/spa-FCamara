626ae66ccf8f2692358655af1cfdf63b38bd7f17
Fabio Beider <fabeider@gmail.com>
2016-11-22 17:21:38 -0200

    Use Http to get products

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule }    from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }   from './app.component';
 import { ProductDetailComponent } from './product-detail.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [
+    BrowserModule, 
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
+  ],
   declarations: [ AppComponent, ProductDetailComponent],
   bootstrap:    [ AppComponent ]
 })
--- app/product.service.ts
+++ app/product.service.ts
@@ -1,11 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 import { Product } from './product';
-import { PRODUCTS } from './mock-products';
 
 @Injectable()
 export class ProductService {
-  getProducts(): Promise<Product[]>{
-    return Promise.resolve(PRODUCTS);
+private productsUrl = 'app/products';
+
+constructor(private http: Http) {}
+
+getProducts(): Promise<Product[]>{
+  return this.http.get(this.productsUrl)
+  .toPromise()
+  .then(response => response.json().data as Product[])
+  .catch(this.handleError);
   }
-}
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+    }
+    }
